db2433b692d2101c5b98daf8071fd4a1ce566aa0
renaesass <renae.sass.sound@gmail.com>
2023-05-14 13:14:13 +1000

    added bootstrap css link

--- src/App.js
+++ src/App.js
@@ -1,3 +1,6 @@
+import "bootstrap/dist/css/bootstrap.css";
+// Put any other imports below so that CSS from your
+// components takes precedence over default styles.
 import "./App.css";
 import Search from "./Search";
 import Forecast from "./Forecast";
